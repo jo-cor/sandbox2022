5ab808073708a4c8b0f337c175fdc9c2a855d645
Jorge Cordoba <jorgecordoba@mtech-systems.com>
2022-08-05 16:26:57 -0500

    up to this: https://angular.io/tutorial/toh-pt6#get-heroes-with-httpclient  ..  duty calls

--- angular-tour-of-heroes/src/app/hero.service.ts
+++ angular-tour-of-heroes/src/app/hero.service.ts
@@ -3,6 +3,7 @@ import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 //https://angular.io/guide/providers
 //https://angular.io/guide/dependency-injection
@@ -10,18 +11,37 @@ import { MessageService } from './message.service';
   providedIn: 'root',
 })
 export class HeroService {
-  constructor(private messagesService: MessageService) {}
+  private heroesUrl = 'api/heroes';
 
+  constructor(
+    private messagesService: MessageService,
+    private http: HttpClient
+  ) {}
+
+  /**
+   * @deprecated The new version takes from HttpClient instead of a simple local array
+   */
   //https://angular.io/tutorial/toh-pt6
-  getHeroes(): Observable<Hero[]> {
+  getHeroes_deprecated(): Observable<Hero[]> {
     const heroes = of(HEROES);
     this.messagesService.add('HeroService: fetched heroes');
     return heroes;
+  }
+
+  /**
+   * Replaces getHeroes_deprecated which used to mock data from a simple local array, this one uses HttpClient
+   */
+  getHeroes(): Observable<Hero[]> {
+    return this.http.get<Hero[]>(this.heroesUrl);
   }
 
   getHero(id: number): Observable<Hero> {
     const hero = HEROES.find((r) => r.id === id)!;
     this.messagesService.add(`HeroService: fetched hero id=${id}`); //The backtick ( ` ) characters define a JavaScript template literal for embedding the id.
     return of(hero);
+  }
+
+  private log(message: string) {
+    this.messagesService.add(`HeroService: ${message}`);
   }
 }
