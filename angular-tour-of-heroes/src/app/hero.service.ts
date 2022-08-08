0893ac29b1f6bcaf4decf19318e270b68ac0b868
Jorge Cordoba <jorgecordoba@mtech-systems.com>
2022-08-08 06:59:48 -0500

    up to getting heroes and hero with httpClient

--- angular-tour-of-heroes/src/app/hero.service.ts
+++ angular-tour-of-heroes/src/app/hero.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, of, catchError, map, tap } from 'rxjs';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
-import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 //https://angular.io/guide/providers
 //https://angular.io/guide/dependency-injection
@@ -20,8 +20,8 @@ export class HeroService {
 
   /**
    * @deprecated The new version takes from HttpClient instead of a simple local array
+   * https://angular.io/tutorial/toh-pt6
    */
-  //https://angular.io/tutorial/toh-pt6
   getHeroes_deprecated(): Observable<Hero[]> {
     const heroes = of(HEROES);
     this.messagesService.add('HeroService: fetched heroes');
@@ -32,16 +32,47 @@ export class HeroService {
    * Replaces getHeroes_deprecated which used to mock data from a simple local array, this one uses HttpClient
    */
   getHeroes(): Observable<Hero[]> {
-    return this.http.get<Hero[]>(this.heroesUrl);
+    return this.http.get<Hero[]>(this.heroesUrl).pipe(
+      tap((_) => this.log('fetched heroes')),
+      catchError(this.handleError<Hero[]>('getHeroes', []))
+    );
+  }
+
+  /**
+   * @deprecated The new version takes from HttpClient instead of a simple local array
+   * https://angular.io/tutorial/toh-pt6#get-hero-by-id
+   */
+  getHero_deprecated(id: number): Observable<Hero> {
+    const hero = HEROES.find((r) => r.id === id)!;
+    this.messagesService.add(`HeroService: fetched hero id=${id}`); //The backtick ( ` ) characters define a JavaScript template literal for embedding the id. you won't make fun of me again, madafaka!
+    return of(hero);
   }
 
   getHero(id: number): Observable<Hero> {
-    const hero = HEROES.find((r) => r.id === id)!;
-    this.messagesService.add(`HeroService: fetched hero id=${id}`); //The backtick ( ` ) characters define a JavaScript template literal for embedding the id.
-    return of(hero);
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get<Hero>(url).pipe(
+      tap((_) => this.log(`fetched hero [${id}]`)),
+      catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
   }
 
   private log(message: string) {
     this.messagesService.add(`HeroService: ${message}`);
+  }
+
+  /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   *
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+      this.log(`[${operation}] failed: ${error.message}.`);
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
   }
 }
